3746296a651f58401db424bb5f22c5350be6eae9
JFuton847 <jimmyfuton847@gmail.com>
2024-05-23 15:40:08 -0500

    test

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+let initialCards = [
+  "object_1",
+  "object_2",
+  "object_3",
+  "object_4",
+  "object_5",
+  "object_6",
+];
+
+let object_1 = {
+  name: "A Path in the Middle of a Forest",
+  link: "https://images.unsplash.com/photo-1715937527911-14a019e0bd95?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+};
+
+let object_2 = {
+  name,
+};
