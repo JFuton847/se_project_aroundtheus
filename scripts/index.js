4b02ff4218da1000a6543d4512fccd4a0b070d67
JFuton847 <jimmyfuton847@gmail.com>
2024-05-31 20:01:10 -0500

    sprint 4 part 3 attempt 1

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,17 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+const profileEditCloseButton = document.querySelector(
+  "#profile-edit-close-button"
+);
+
+profileEditCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
